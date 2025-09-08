f43dbc6669de39228af689393ce337ca8787a727
Niklas Pfund <stu217120@informatik.uni-kiel.de>
2025-09-08 19:21:56 +0200

    test localstorage

--- Clue/main.js
+++ Clue/main.js
@@ -1,7 +1,20 @@
-function main() {
+function showNumPlay() {
     if (isNaN(Number(localStorage.numPlayers))) {
         document.getElementById("screenNumPlayers").innerHTML = "Number of players is not set!"
     } else {
         document.getElementById("screenNumPlayers").innerHTML = Number(localStorage.numPlayers)
     }
+}
+
+function test() {
+    const test = {
+        bloom: "-"
+    };
+    localStorage.comb = test;
+    document.getElementById("screenTest").innerHTML = localStorage.comb;
+}
+
+function main() {
+    showNumPlay()
+    test()
 }
